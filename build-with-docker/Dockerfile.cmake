ced20e4550eda7b8e8d23694eb19b1a57ef22369
Roddy González <rbg@mega.co.nz>
2025-12-10 12:49:13 +0100

    Use only 4 cores for docker builds

--- build-with-docker/Dockerfile.cmake
+++ build-with-docker/Dockerfile.cmake
@@ -78,7 +78,7 @@ RUN --mount=type=cache,target=/tmp/ccache \
     -DENABLE_TSAN=${ENABLE_tsan} \
     -DENABLE_MEGACMD_TESTS=${ENABLE_MEGACMD_TESTS} \
     -DWITH_FUSE=ON \
-    && cmake --build /tmp/build -j$(nproc) --target mega-cmd mega-cmd-server mega-exec \
+    && cmake --build /tmp/build -j4 --target mega-cmd mega-cmd-server mega-exec \
     mega-cmd-updater mega-cmd-tests-integration mega-cmd-tests-unit \
     && cmake --install /tmp/build #|| mkdir /inspectme && mv /tmp/build/* /vcpkg  /inspectme
 
